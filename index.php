27e78a90acb285bac096fdb9f39ac31e1b02ca70
Alejandro Manuel <alejandro.dlcescate@gmail.com>
2026-01-13 21:54:13 -0500

    seo 4

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-<!DOCTYPE html>
-<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,6 +10,7 @@
     <title>Colegio San Isidro Ica | Inicial, Primaria y Secundaria | Matrícula 2026</title>
 
     <link rel="icon" type="image/png" href="img/icono.webp">
+    <link rel="shortcut icon" href="img/icono.webp">
     <link rel="apple-touch-icon" href="img/icono.webp">
     <meta name="google-site-verification" content="kl1Jd-2ozsH2p1Xb4w9M_wVaCLx_q2dCTjDD5gB1enw" />
     <!-- META DESCRIPTION OPTIMIZADA -->
@@ -35,6 +34,7 @@
     <link rel="canonical" href="https://www.csi.edu.pe/">
 
     <!-- OPEN GRAPH (REDES SOCIALES) -->
+    <meta property="og:site_name" content="Colegio San Isidro Ica">
     <meta property="og:title" content="Colegio San Isidro Ica | Educación Inicial, Primaria y Secundaria">
     <meta property="og:description" content="Institución educativa líder en Ica. Matrícula 2026 abierta.">
     <meta property="og:image" content="https://www.csi.edu.pe/img/icono.webp">
@@ -70,6 +70,7 @@
       ]
     }
     </script>
+</head>
 <body>
 
     <div class="top-bar">
